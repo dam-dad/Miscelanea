285799104b2fe03e840b0c30b22c4d6e48cfa34c
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-10-21 09:57:03 +0100

    U ejemplo de leer fichero mejorado

--- src/main/java/dad/javafx/miscelanea/ficheros/LeerFichero.java
+++ src/main/java/dad/javafx/miscelanea/ficheros/LeerFichero.java
@@ -4,11 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 public class LeerFichero {
 	
-	public static void leerLineaALinea(File fichero) throws IOException {
-		FileReader fr = new FileReader(fichero);
+	public static List<String> leerLineaALinea(File fichero, Charset charset) throws IOException {
+		List<String> lines = new ArrayList<String>();
+		FileReader fr = new FileReader(fichero, charset);
 		BufferedReader br = new BufferedReader(fr);
 		String line;
 		while ((line = br.readLine()) != null) {
@@ -16,10 +22,19 @@ public class LeerFichero {
 		}
 		br.close();
 		fr.close();
+		return lines;
 	}
-
+	
 	public static void main(String[] args) throws IOException {
-		leerLineaALinea(new File("datos.csv"));
+		
+		// usando implementación propia
+		List<String> lines1 = leerLineaALinea(new File("datos.csv"), Charset.forName("UTF-8"));
+		lines1.stream().forEach(l -> System.out.println(l));
+		
+		// usando librería commons-io
+		List<String> lines2 = FileUtils.readLines(new File("datos.csv"), Charset.forName("UTF-8"));
+		lines2.stream().forEach(l -> System.out.println(l));
+		
 	}
 
 }
