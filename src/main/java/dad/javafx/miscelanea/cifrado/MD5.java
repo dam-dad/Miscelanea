f55f2678fe877a8333a90816fd538cb689cdb1ef
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-10-21 10:18:14 +0100

    A prueba md5

---
+++ src/main/java/dad/javafx/miscelanea/cifrado/MD5.java
@@ -0,0 +1,15 @@
+package dad.javafx.miscelanea.cifrado;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class MD5 {
+
+	public static void main(String[] args) {
+		
+		String password = "norris";
+		String md5 = DigestUtils.md5Hex(password).toUpperCase();
+		System.out.println("Password cifrado: " + md5);
+		
+	}
+
+}
